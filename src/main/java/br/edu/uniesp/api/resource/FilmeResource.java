2e231b9d685e534eedc791bdba3aa4c23e2700ab
Lucassantosalls <lucas.santos.al.ls@gmail.com>
2022-03-29 20:54:48 -0300

    primeiro commit

---
+++ src/main/java/br/edu/uniesp/api/resource/FilmeResource.java
@@ -0,0 +1,41 @@
+package br.edu.uniesp.api.resource;
+
+import br.edu.uniesp.api.model.Filme;
+import br.edu.uniesp.api.repository.FilmeRepository;
+import br.edu.uniesp.api.service.FilmeService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/filme")
+public class FilmeResource {
+
+    @Autowired
+    private FilmeService service;
+
+
+    @PostMapping
+    public Filme salvar(@RequestBody Filme filme){
+        filme = service.salvar(filme);
+        return filme;
+    }
+
+    @GetMapping
+    public String listar(){
+
+        return "Deveria Retornar Lista";
+    }
+
+    @DeleteMapping
+    public String remover(){
+
+        return "Removendo";
+    }
+
+    @PutMapping
+    public String atualizar(){
+
+        return "Atualizando";
+    }
+
+}
---
+++ src/main/java/br/edu/uniesp/api/resource/GeneroResource.java
@@ -0,0 +1,24 @@
+package br.edu.uniesp.api.resource;
+
+import br.edu.uniesp.api.model.Filme;
+import br.edu.uniesp.api.model.Genero;
+import br.edu.uniesp.api.service.GeneroService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/genero")
+public class GeneroResource {
+
+    @Autowired
+    private GeneroService service;
+
+    @PostMapping
+    public Genero salvar(@RequestBody Genero genero){
+        genero = service.salvar(genero);
+        return genero;
+    }
+}
